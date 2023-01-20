0f3fcb319f969274bbfb83913340fbfd7a8c1630
Iryna <aripluss@i.ua>
2023-01-21 01:35:07 +0200

    Fix film-card markup

--- src/js/film-card.js
+++ src/js/film-card.js
@@ -1,25 +1,32 @@
 'use strict';
 
 export function createFilmCards(films) {
-  const { poster_path, title, release_date, vote_average } = films;
-
   return films
     .map((film, idx) => {
+      const { poster_path, title, release_date, vote_average, genreNames } =
+        film;
+
       return `
         <li class="film-card__item">
-            <img src="${poster_path}" alt="${
-        title + 'poster'
-      }" class="film-card__poster" />
+            <img src="${
+              'https://image.tmdb.org/t/p/original' + poster_path
+            }" alt="${title + ' poster'}" class="film-card__poster" />
             <div class="film-card__content">
                 <h2 class="film-card__title">${title}</h2>
                 <p class="film-card__genre">${
-                  film.genreNames.length > 2
-                    ? film.genreNames.slice(0, 2) +
-                      `<span data-id="${idx}" class="js-other-films">Other</span>`
-                    : film.genreNames
+                  genreNames.length > 2
+                    ? genreNames.slice(0, 2).join(', ') +
+                      `<span data-id="${idx}" class="js-other-films">, Other</span>`
+                    : genreNames
                 }
-                    <span class="film-card__release-date">${release_date}</span>
-                    <span class="film-card__rating">${vote_average}</span>
+                    <span class="film-card__release-date">${
+                      release_date === ''
+                        ? ''
+                        : new Date(release_date).getFullYear()
+                    }
+                    </span>
+                    <span class="film-card__rating">${vote_average.toFixed(1)}
+                    </span>
                 </p>
             </div>
         </li>
