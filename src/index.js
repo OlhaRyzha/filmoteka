1e75febcb8eeae8ff784aabd3e49ab4bc067298f
OlgaRyzha <orizha11@gmail.com>
2023-01-21 11:47:34 +0200

    add pagination folders

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './js/modalFooterTeam';
 import './js/api';
 import './js/film-card';
 import './js/films-gallery';
+import './js/pagination';
 import './js/films-gallery-trends';
 
---
+++ src/js/pagination.js
@@ -0,0 +1,31 @@
+// import Pagination from 'tui-pagination';
+// import 'tui-pagination/dist/tui-pagination.css';
+// const container = document.getElementById('pagination');
+// const options = {
+//   totalItems: 17,
+//   itemsPerPage: 10,
+//   visiblePages: 17,
+//   page: 1,
+//   centerAlign: false,
+//   firstItemClassName: 'tui-first-child',
+//   lastItemClassName: 'tui-last-child',
+//   template: {
+//     page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+//     currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+//     moveButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}">' +
+//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</a>',
+//     disabledMoveButton:
+//       '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+//         '<span class="tui-ico-{{type}}">{{type}}</span>' +
+//       '</span>',
+//     moreButton:
+//       '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+//         '<span class="tui-ico-ellip">...</span>' +
+//       '</a>'
+//   }
+// };
+
+// const pagination = new Pagination(container, options);
+
