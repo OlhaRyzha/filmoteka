12e042a3694bf601b0bef79e6148b610c9b5bfc5
OlgaRyzha <orizha11@gmail.com>
2023-01-24 06:43:16 +0200

    видалила git push -u origin feature/preloader

--- src/js/films-gallery.js
+++ src/js/films-gallery.js
@@ -1,7 +1,10 @@
 'use strict';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 import { showLoader, hideLoader } from './loaders';
 import { ThemoviedbAPI } from './api';
 import { createFilmCards } from './film-card';
+
 
 const themoviedbAPI = new ThemoviedbAPI();
 
@@ -9,6 +12,7 @@ const galleryEl = document.querySelector('.film-card__list');
 const formEl = document.querySelector('.search-form#home-page');
 const submitBtnEl = document.querySelector('.search-form .search-btn');
 const errorMessage = document.querySelector('.warning');
+const container = document.querySelector('#pagination');
 
 formEl.addEventListener('submit', onFormSubmit);
 
@@ -33,7 +37,44 @@ async function onFormSubmit(event) {
     const genres = await themoviedbAPI.fetchGenresMovieList();
 
     const filmsByQuery = await themoviedbAPI.fetchFilmByQuery();
-    const { results } = filmsByQuery;
+    const { results, total_results } = filmsByQuery;
+    console.log(total_results)
+    async function getPagination() {
+      try {
+       
+        const options = {
+          totalItems: total_results < 20 ? total_results : 200,
+          itemsPerPage: 20,
+          visiblePages: 5,
+        };
+    
+        const pagination = new Pagination(container, options);
+    
+        pagination.on('afterMove', function (eventData) {
+    
+          themoviedbAPI.page = eventData.page;
+    
+          themoviedbAPI
+            .fetchFilmByQuery()
+            .then(data => {
+              data.results.map(film => {
+                film.genreNames = film.genre_ids.map(genreId => {
+                  return themoviedb.genresObject[genreId] || 'Unknown genre';
+                });
+              });
+    
+              galleryEl.innerHTML = createFilmCards(data.results);
+            })
+            .catch(err => {
+              console.log(err);
+            });
+        });
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    
+    getPagination();
 
     if (results.length === 0) {
       errorMessage.classList.remove('is-hidden');
