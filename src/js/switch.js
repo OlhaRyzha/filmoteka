46d161172603286d52181d88ac4d003671fdab6e
nastya-koptilova <koptilova.nastya@gmail.com>
2023-01-24 11:32:21 +0200

    add modal in library

--- src/js/switch.js
+++ src/js/switch.js
@@ -1,37 +1,37 @@
 'use strict';
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-const switchInput = document.querySelector('#theme-switch-toggle'); 
+const switchInput = document.querySelector('#theme-switch-toggle');
 const bodyEl = document.querySelector('body');
 
 // console.log(bodyEl)
 
-  switchInput.addEventListener('change', changeTheme);
+switchInput.addEventListener('change', changeTheme);
 
-  function changeTheme() {
-    bodyEl.classList.toggle(Theme.DARK);
-    bodyEl.classList.toggle(Theme.LIGHT);
-  
-    getCurrentTheme(bodyEl.classList);
-  }
- 
-  function getCurrentTheme(currentTheme) {
-    localStorage.setItem('Theme', currentTheme);
-  }
-  
-  function actualTheme() {
-    const savedTheme = localStorage.getItem('Theme');
+function changeTheme() {
+  bodyEl.classList.toggle(Theme.DARK);
+  bodyEl.classList.toggle(Theme.LIGHT);
 
-    if (savedTheme === Theme.DARK) {
-      bodyEl.classList.add(savedTheme);
-      switchInput.checked = true;
-    } else {
-      bodyEl.classList.add(Theme.LIGHT);
-    }
-  }
+  getCurrentTheme(bodyEl.classList);
+}
 
-  actualTheme();
+function getCurrentTheme(currentTheme) {
+  localStorage.setItem('Theme', currentTheme);
+}
+
+function actualTheme() {
+  const savedTheme = localStorage.getItem('Theme');
+
+  if (savedTheme === Theme.DARK) {
+    bodyEl.classList.add(savedTheme);
+    switchInput.checked = true;
+  } else {
+    bodyEl.classList.add(Theme.LIGHT);
+  }
+}
+
+actualTheme();
