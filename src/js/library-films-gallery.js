f5de8a72ca699b9637ca840c0d3c2bf5d26d24aa
OlgaRyzha <orizha11@gmail.com>
2023-01-24 14:48:41 +0200

    +

--- src/js/library-films-gallery.js
+++ src/js/library-films-gallery.js
@@ -1,24 +1,29 @@
 'use strict';
 import { showLoader, hideLoader } from './loaders';
 import localStorageService from './localStorage';
-import { createFilmCards } from './film-card';
+import { createCardById } from './library-create-card';
+import { ThemoviedbAPI } from './api';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
 
 const galleryEl = document.querySelector('.film-card__list');
 const watchedBtnEl = document.querySelector('.js-watched');
 const queueBtnEl = document.querySelector('.js-queue');
+const container = document.querySelector('#pagination');
+
+const theMovieById = new ThemoviedbAPI();
 
 if (!watchedBtnEl || !queueBtnEl) {
   return;
 }
 
+watchedBtnEl.addEventListener('click', onWatchedBtnClick);
+queueBtnEl.addEventListener('click', onQueueBtnClick);
 
-// watchedBtnEl.addEventListener('click', onWatchedBtnClick);
-// queueBtnEl.addEventListener('click', onQueueBtnClick);
-
-function onWatchedBtnClick(event) {
+async function onWatchedBtnClick(event) {
+  galleryEl.innerHTML = '';
   showLoader();
-
-  const watchedMovies = localStorageService.load('watched');
+ const watchedMovies = localStorageService.load('watched');
   console.log(watchedMovies);
 
   if (watchedMovies === undefined) {
@@ -26,26 +31,99 @@ function onWatchedBtnClick(event) {
     return;
   }
 
-  for (const prop in watchedMovies) {
-    galleryEl.insertAdjacentHTML('beforeend', createFilmCards(prop));
-  }
+  watchedMovies.map(el => {
+    theMovieById.fetchFilmInfo(el).then(data => {
+      // console.log();
+      const arrId = data.genres.map(el => el.id);
+      data.genreNames = arrId.map(genreId => {
+        return theMovieById.genresObject[genreId] || 'Unknown genre';
+      });
+
+      // console.log(card)
+      galleryEl.insertAdjacentHTML('afterbegin', createCardById(data));
+
+      async function getPagination() {
+        try {
+          const results = watchedMovies.length;
+      
+          const options = {
+            totalItems: results,
+            itemsPerPage: 20,
+            visiblePages: 5,
+          };
+      
+          const pagination = new Pagination(container, options);
+      
+          pagination.on('afterMove', function (eventData) {
+      
+            theMovieById.page = eventData.page;
+      
+            galleryEl.innerHTML = createCardById(data);
+             
+          });
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+      
+      getPagination();
+      
+    });
+  });
 
   hideLoader();
 }
 
-function onQueueBtnClick(event) {
+async function onQueueBtnClick(event) {
+  galleryEl.innerHTML = '';
   showLoader();
 
-  const moviesInQueue = localStorageService.load('queue');
+ const queuedMovies = localStorageService.load('queue');
 
-  if (moviesInQueue === undefined) {
+
+  if (queuedMovies === undefined) {
     hideLoader();
     return;
   }
 
-  for (const prop in moviesInQueue) {
-    galleryEl.insertAdjacentHTML('beforeend', createFilmCards(prop));
-  }
+  queuedMovies.map(el => {
+    theMovieById.fetchFilmInfo(el).then(data => {
+  
+      const arrId = data.genres.map(el => el.id);
+      data.genreNames = arrId.map(genreId => {
+        return theMovieById.genresObject[genreId] || 'Unknown genre';
+      });
+
+      // console.log(card)
+      galleryEl.insertAdjacentHTML('afterbegin', createCardById(data));
+
+      async function getPagination() {
+        try {
+          const results = queuedMovies.length;
+      
+          const options = {
+            totalItems: results,
+            itemsPerPage: 20,
+            visiblePages: 5,
+          };
+      
+          const pagination = new Pagination(container, options);
+      
+          pagination.on('afterMove', function (eventData) {
+      
+            theMovieById.page = eventData.page;
+      
+            galleryEl.innerHTML = createCardById(data);
+             
+          });
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
+      
+      getPagination();
+    });
+  });
 
   hideLoader();
 }
