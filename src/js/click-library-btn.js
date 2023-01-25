ddbca009f4730b39ed8e97b4b0fa316232d5aeed
OlgaRyzha <orizha11@gmail.com>
2023-01-25 15:20:09 +0200

    fix pagination

--- src/js/click-library-btn.js
+++ src/js/click-library-btn.js
@@ -6,33 +6,38 @@ import { createCardInfo } from './card-info';
 import localStorageService from './localstorage.js';
 import { createCardById } from './library-create-card';
 import { showLoader, hideLoader } from './loaders';
-import {getPagination}  from './pagination-ilibrary';
+import { getPagination } from './pagination-ilibrary';
 
 const galleryEl = document.querySelector('.library-film-card__list');
 const libraryBtn = document.querySelector('.library__js');
 const container = document.querySelector('#pagination');
-const footer = document.querySelector('.footer__container');
-
-
+const footer = document.querySelector('.footer');
+const main = document.querySelector('main');
+const watchedBtnEl = document.querySelector('.js-watched');
+const queueBtnEl = document.querySelector('.js-queue');
 const theMovieById = new ThemoviedbAPI();
-onLibraryBtnClick()
+onLibraryBtnClick();
 
- 
-export async function onLibraryBtnClick(){
+export async function onLibraryBtnClick() {
   showLoader();
- 
-  galleryEl.innerHTML ='';
+  queueBtnEl.classList.remove('is-active-btn');
+  watchedBtnEl.classList.add('is-active-btn');
+  galleryEl.innerHTML = '';
   const watchedMovies = localStorageService.load('watched');
- 
-  if(!watchedMovies){
+
+  if (!watchedMovies) {
     Notify.failure(
       'Sorry, there are no films matching your search query. Please try again.'
     );
+
     hideLoader();
 
-    galleryEl.innerHTML = `<div class="info-card">
+    main.innerHTML = `<div class="info-card">
     <p>you don't have any movies to watch yet((</p>
-  </div>`
+  </div>`;
+    // main.style.backgroundColor = 'var(--bg-color)';
+    main.style.height = '738px';
+
     container.classList.add('visually-hidden');
     footer.style.position = 'fixed';
     footer.style.bottom = '0';
@@ -42,21 +47,16 @@ export async function onLibraryBtnClick(){
   hideLoader();
   watchedMovies.map(el => {
     theMovieById.fetchFilmInfo(el).then(data => {
-
       const arrId = data.genres.map(el => el.id);
       data.genreNames = arrId.map(genreId => {
         return theMovieById.genresObject[genreId] || 'Unknown genre';
       });
 
-
       galleryEl.insertAdjacentHTML('afterbegin', createCardById(data));
 
       const results = watchedMovies.length;
 
-      getPagination(results, data)
-      
+      getPagination(results, data);
     });
   });
-
-  
 }
--- src/js/library-films-gallery.js
+++ src/js/library-films-gallery.js
@@ -5,82 +5,58 @@ import { showLoader, hideLoader } from './loaders';
 import localStorageService from './localStorage';
 import { createCardById } from './library-create-card';
 import { ThemoviedbAPI } from './api';
-import {getPagination}  from './pagination-ilibrary';
+import { getPagination } from './pagination-ilibrary';
+import { onLibraryBtnClick } from './click-library-btn';
 
 const galleryEl = document.querySelector('.library-film-card__list');
 const watchedBtnEl = document.querySelector('.js-watched');
 const queueBtnEl = document.querySelector('.js-queue');
 const container = document.querySelector('#pagination');
-const footer = document.querySelector('.footer__container');
+const footer = document.querySelector('.footer');
+const main = document.querySelector('main');
 
 const theMovieById = new ThemoviedbAPI();
 
-
-
-watchedBtnEl.addEventListener('click', onWatchedBtnClick);
+watchedBtnEl.addEventListener('click', onLibraryBtnClick);
 queueBtnEl.addEventListener('click', onQueueBtnClick);
 
-async function onWatchedBtnClick(event) {
-  galleryEl.innerHTML = '';
-  showLoader();
-  queueBtnEl.classList.remove('is-active-btn');
-  watchedBtnEl.classList.add('is-active-btn');
-
- const watchedMovies = localStorageService.load('watched');
-createMovieCard(watchedMovies)
-}
-
-
-async function onQueueBtnClick(event) {
-  galleryEl.innerHTML = '';
-  showLoader();
+async function onQueueBtnClick() {
+  // showLoader();
   queueBtnEl.classList.add('is-active-btn');
   watchedBtnEl.classList.remove('is-active-btn');
+  galleryEl.innerHTML = '';
+  const queuedMovies = localStorageService.load('queue');
 
- const queuedMovies = localStorageService.load('queue');
-
-createMovieCard(queuedMovies)
-}
-
-
-
-async function createMovieCard(arr){
-  if(!arr){
-    hideLoader();
+  if (!queuedMovies) {
     Notify.failure(
-      'Sorry, there are no filmes matching your search query. Please try again.'
+      'Sorry, there are no films matching your search query. Please try again.'
     );
+
     hideLoader();
 
-    galleryEl.innerHTML = `<div class="info-card">
+    main.innerHTML = `<div class="info-card">
     <p>you don't have any movies to watch yet((</p>
-  </div>`
+  </div>`;
+    main.style.height = '738px';
     container.classList.add('visually-hidden');
     footer.style.position = 'fixed';
     footer.style.bottom = '0';
     footer.style.left = '50%';
     footer.style.transform = 'translateX(-50%)';
-  }else{
-   arr.map(el => {
-      theMovieById.fetchFilmInfo(el).then(data => {
-  
-        const arrId = data.genres.map(el => el.id);
-        data.genreNames = arrId.map(genreId => {
-          return theMovieById.genresObject[genreId] || 'Unknown genre';
-        });
-  
-  
-        galleryEl.insertAdjacentHTML('afterbegin', createCardById(data));
-  
-        const results = arr.length;
-
-         getPagination(results, data)
-        
-      });
-    });
   }
-  
- 
-
   hideLoader();
+  queuedMovies.map(el => {
+    theMovieById.fetchFilmInfo(el).then(data => {
+      const arrId = data.genres.map(el => el.id);
+      data.genreNames = arrId.map(genreId => {
+        return theMovieById.genresObject[genreId] || 'Unknown genre';
+      });
+
+      galleryEl.insertAdjacentHTML('afterbegin', createCardById(data));
+
+      const results = queuedMovies.length;
+
+      getPagination(results, data);
+    });
+  });
 }
